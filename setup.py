40c6ccacf77d0d1a4f604fc895a6a588ef5fcb1c
Michael Delgado <mdelgado@rhg.com>
2017-02-10 22:58:12 -0800

    add python 3 support

--- setup.py
+++ setup.py
@@ -51,6 +51,10 @@ setup(
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.5'
     ],
     test_suite='tests',
     setup_requires=['pytest-runner'],
