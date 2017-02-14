6ddaf77adb3a3d1ad42eee06aae657fe15f77fa7
Michael Delgado <mdelgado@rhg.com>
2017-02-13 17:36:38 -0800

    revert to assertions

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -3,7 +3,4 @@ import doctest
 
 def test_readme():
     errs, _ = doctest.testfile('../README.rst', report=True)
-    if errs > 0:
-        raise ValueError(
-            '{} errors encountered in README.rst'.format(
-                errs))
+    assert not errs
